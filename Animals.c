fa2b82ee1cfb735dd0395115010fa0a2580561cc
Kevin-1chuli <culikevin42@gmail.com>
2025-04-05 14:55:35 +0400

    validation of the code

--- Animals.c
+++ Animals.c
@@ -1,35 +1,76 @@
--u #include <stdio.h>
-int main(){
-int N,X;
+#include <stdio.h>
+#include <ctype.h>
 
-printf("\nEnter the number of domestic animals: ");
-scanf("%d",&N);
-char  domestic[N][50];
-
-//inputf domestic animals
-printf("Enter the number of domestic %d animals:\n",N);
-for(int i = 0; i < N; i++){
-    scanf("%s",domestic[i]);
+// Function to ensure that no digit is entered in the animal names
+void getAnimalNames(char animals[][50], int count) {
+    for (int i = 0; i < count; i++) {
+        while (1) {
+            printf("Enter name for animal %d: ", i + 1);
+            scanf("%s", animals[i]);
+            
+            int valid = 1;
+            // Check each character to see if it is a digit
+            for (int j = 0; animals[i][j] != '\0'; j++) {
+                if (isdigit(animals[i][j])) {
+                    valid = 0;
+                    break;
+                }
+            }
+            if (valid) {
+                break;  // Exit the loop if the name is valid
+            } else {
+                printf("Invalid input! Animal names shouldn't contain digits. Try again.\n");
+            }
+        }
+    }
 }
 
-//get the no. of wild animals
-printf("\nEnter the number of wild animals: ");
-scanf("%d",&X);
-char wild[X][50];
+int main() {
+    int N, X;
 
-//input wild animals
-printf("Enter the number of domestic %d animals:\n",X);
-for(int i = 0; i < X; i++){
-    scanf("%s",wild[i]);
-}
+    // Ensure that the number of domestic animals is a positive integer
+    while (1) {
+        printf("\nEnter the number of domestic animals: ");
+        if (scanf("%d", &N) == 1 && N > 0) {
+            break;
+        } else {
+            printf("Invalid input! Please enter a number.\n");
+            while (getchar() != '\n');  // clear the input buffer
+        }
+    }
 
-printf("\nList of all animals:\n");
-for(int i = 0; i < N; i++){
-    printf("%s\n",domestic[i]);
-}
-for(int i = 0; i < X; i++ ){
-   printf("%s\n",wild[i]);
-}
+    // Ensure that the number of wild animals is a positive integer
+    while (1) {
+        printf("\nEnter the number of wild animals: ");
+        if (scanf("%d", &X) == 1 && X > 0) {
+            break;
+        } else {
+            printf("Invalid input! Please enter a number.\n");
+            while (getchar() != '\n');  // clear the input buffer
+        }
+    }
+
+    // Declare arrays to hold animal names (each name can be up to 49 characters + '\0')
+    char domestic[N][50];
+    char wild[X][50];
+
+    // Get the names of domestic animals
+    printf("\nEnter domestic animal names:\n");
+    getAnimalNames(domestic, N);
+
+    // Get the names of wild animals
+    printf("\nEnter wild animal names:\n");
+    getAnimalNames(wild, X);
+
+    // Print all animals
+    printf("\nDomestic animals:\n");
+    for (int i = 0; i < N; i++) {
+        printf("%s\n", domestic[i]);
+    }
+    for (int i = 0; i < X; i++) {
+        printf("%s\n", wild[i]);
+    }
+
 
     return 0;
 }
